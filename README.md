39cb2962df7595f15ead5756e12b8cd95ddf4a88
echosys <gel@rose-hulman.edu>
2019-04-24 21:16:58 -0700

    update dependencies
    update dependencies

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # cse573demo
+
+
+
+
+To run the demo, please make sure you have the python dependencies. There is a requirement.txt file that manages all the dependencies with virtual env. Also we can use the bash script to install them.
---
+++ bash.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install python
+sudo pip install numpy
+sudo pip install sklearn
+sudo pip install gensim
+sudo pip install nltk
+sudo pip install matplotlib
+sudo pip install seaborn
+sudo pip install bokeh
+sudo python ./code/code_573.py
+#alternatively
+#pip install -r requirements.txt
